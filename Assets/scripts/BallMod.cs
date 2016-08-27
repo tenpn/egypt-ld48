3995c9bf401e54d917e0cf8374ae785a3743b8c4
Andrew Fray <andrew@spryfox.com>
2016-08-27 23:59:28 +0100

    fix points/mass mods

--- Assets/scripts/BallMod.cs
+++ Assets/scripts/BallMod.cs
@@ -3,7 +3,7 @@ using System;
 
 [Serializable]
 public class BallMod {
-    public float PointsMul = 0f;
+    public float PointsMul = 1f;
     public float PowerAdd = 0f;
-    public float MassMul = 0f;
+    public float MassMul = 1f;
 }
