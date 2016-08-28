0c72860575e13d3cbbd6c921d91ea8017640da96
Andrew Fray <andrew@spryfox.com>
2016-08-28 18:36:35 +0100

    PlayerModsLabels so you can see what your next ball will have

---
+++ Assets/scripts/PlayerModsLabel.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(Text))]
+class PlayerModsLabel : MonoBehaviour {
+
+    //////////////////////////////////////////////////
+
+    Text label;
+
+    //////////////////////////////////////////////////
+
+    void Awake() {
+        label = GetComponent<Text>();
+        
+        var activeMatch = FindObjectOfType<Match>();
+        activeMatch.ModsChanged += OnModsChanged;
+        label.text = "";
+    }
+
+    void OnModsChanged(IEnumerable<BallMod> mods) {
+        label.text = mods.SummariseMods();
+    }
+}
